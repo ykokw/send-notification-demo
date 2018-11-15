ba3d973134ca41ea0c86ff4dc59ff9940b2fbd98
ykokw <0k4w4.yuk1@gmail.com>
2018-11-15 12:09:55 +0900

    Refactor

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,25 @@
-const { Expo } = require("expo-server-sdk");
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const { Expo } = require('expo-server-sdk');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 admin.initializeApp();
 
 const expo = new Expo();
 
 exports.sendNotification = functions.firestore
-  .document("messages/{messageId}")
+  .document('messages/{messageId}')
   .onCreate(async () => {
-    console.log("onCreate");
+    // Firestoreのインスタンスを用意
     const settings = { timestampsInSnapshots: true };
     const db = admin.firestore();
     db.settings(settings);
-    const query = db.collection("tokens").where("token", ">", "");
+
+    // デバイストークンをFirestoreから全検索
+    const query = db.collection('tokens').where('token', '>', '');
     const snapshot = await query.get();
-    const tokens = [];
-    snapshot.forEach(doc => tokens.push(doc.get("token")));
-    console.log(JSON.stringify(tokens));
+    const tokens = snapshot.docs.map(doc => doc.get('token'));
+
+    // プッシュ通知用のメッセージオブジェクトを作成
     const messages = [];
     tokens.forEach(pushToken => {
       if (!Expo.isExpoPushToken(pushToken)) {
@@ -27,26 +29,20 @@ exports.sendNotification = functions.firestore
       // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
       messages.push({
         to: pushToken,
-        sound: "default",
-        body: "This is a test notification",
-        data: { withSome: "data" }
+        sound: 'default',
+        body: 'This is a test notification',
+        data: { withSome: 'data' },
       });
     });
-    let chunks = expo.chunkPushNotifications(messages);
-    let tickets = [];
+    const chunks = expo.chunkPushNotifications(messages);
+    const tickets = [];
     (async () => {
-      // Send the chunks to the Expo push notification service. There are
-      // different strategies you could use. A simple one is to send one chunk at a
-      // time, which nicely spreads the load out over time:
+      // Expo Push APIをリクエスト
       for (let chunk of chunks) {
         try {
-          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
           console.log(ticketChunk);
           tickets.push(...ticketChunk);
-          // NOTE: If a ticket contains an error code in ticket.details.error, you
-          // must handle it appropriately. The error codes are listed in the Expo
-          // documentation:
-          // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
         } catch (error) {
           console.error(error);
         }
